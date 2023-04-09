c5ae41990c6f10d6f7a915ec70dee25e5e830d96
Avichay3 <101700010+Avichay3@users.noreply.github.com>
2023-04-10 01:16:28 +0300

    second commit, start some functions

--- ex1_utils.py
+++ ex1_utils.py
@@ -8,11 +8,15 @@
          ########: ##:::. ##::'######:
         ........::..:::::..:::......::
 """
+import sys
 from typing import List
 
 import numpy as np
+
+import cv2
 LOAD_GRAY_SCALE = 1
 LOAD_RGB = 2
+RGB2YIQ_mat = np.array([0.258, 0.347, 0.133, 0.583, -0.333, -0.456, 0.211, 0.233, 0.521]).reshape(3, 3)
 
 
 def myID() -> np.int:
@@ -30,7 +34,18 @@ def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
     :param representation: GRAY_SCALE or RGB
     :return: The image object
     """
-    pass
+    img = cv2.imread(filename)
+    if img is None:
+        sys.exit("cannot read the image")
+    if representation == 1:   ## means that the image is in gray scale
+        img = img.convert('L') ## make it gray scale
+    if representation == 2:
+        img = img.convert('RGB') ## convert it to RGB colors
+    img = np.asarray(img) ## after convert the image to gray/RGB scale, need to make it as numpy array
+    img = img.astype(np.float) ## convert it to floating point array
+    norm_img = normalizeData(img) ## normalize to the same scale of values the numpy array with the floating point
+    return norm_img
+
 
 
 def imDisplay(filename: str, representation: int):
@@ -40,7 +55,11 @@ def imDisplay(filename: str, representation: int):
     :param representation: GRAY_SCALE or RGB
     :return: None
     """
-    pass
+    image = imReadAndConvert(filename , representation) ## step 1, convert the image into numpy array
+    cv2.imshow(filename, image)  ## display the image
+    cv2.waitKey(0) ## wait for user to press something in keybord
+    cv2.destoryAllWindows()
+
 
 
 def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
@@ -49,7 +68,11 @@ def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
     :param imgRGB: An Image in RGB
     :return: A YIQ in image color space
     """
-    pass
+    origin_shape = imgRGB.shape ##the RGB representation of the image
+    imgRGB = imgRGB.reshape(-1, 3)
+    YIQ_img = imgRGB.dot(RGB2YIQ_mat).reshape(origin_shape)
+    return YIQ_img
+
 
 
 def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
@@ -79,3 +102,6 @@ def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarr
         :return: (List[qImage_i],List[error_i])
     """
     pass
+
+def normalizeData(data):
+    return (data - np.min(data)) / (np.max(data) - np.min(data))
